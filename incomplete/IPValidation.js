493dac33dc920c51cfa6f36ff744a8fd263143b9
Johnny <jay801023@gmail.com>
2024-06-22 12:16:50 -0700

    Partially completed IPValidation

--- incomplete/IPValidation.js
+++ incomplete/IPValidation.js
@@ -16,3 +16,40 @@
 // Notes:
 // Leading zeros (e.g. 01.02.03.04) are considered invalid
 // Inputs are guaranteed to be a single string
+
+// First attempt without regex
+function isValidIP(str) {
+  let arr = str.split(".");
+  if (arr.length != 4) return false;
+
+  const conditions = [" ", "\n", "\r", "\t", 'e'];
+  let invalid = arr.some((val) => {
+    return (
+      !val ||
+      isNaN(+val) ||
+      conditions.some((e) => val.includes(e)) ||
+      (val.length > 1 && val[0] === '0') ||
+      +val > 255 ||
+      +val < 0
+    );
+  });
+  return !invalid;
+}
+
+console.log(isValidIP("0.0.0.0")); // true);
+console.log(isValidIP("12.255.56.1")); // true);
+console.log(isValidIP("137.255.156.100")); // true);
+console.log(isValidIP("")); //false);
+console.log(isValidIP("abc.def.ghi.jkl")); //false);
+console.log(isValidIP("123.456.789.0")); //false);
+console.log(isValidIP("12.34.56")); //false);
+console.log(isValidIP("01.02.03.04")); //false);
+console.log(isValidIP("256.1.2.3")); //false);
+console.log(isValidIP("1.2.3.4.5")); //false);
+console.log(isValidIP("123,45,67,89")); //false);
+console.log(isValidIP("1e0.1e1.1e2.2e2")); //false);
+console.log(isValidIP(" 1.2.3.4")); //false);
+console.log(isValidIP("1.2.3.4 ")); //false);
+console.log(isValidIP("12.34.56.-7")); //false);
+console.log(isValidIP("1.2.3.4\n")); //false);
+console.log(isValidIP("\n1.2.3.4")); //false);
